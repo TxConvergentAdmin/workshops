295812ff0a57c4943603706b8966e7ed928a6961
sanakohli <eng@txconvergent.org>
2025-02-24 17:39:13 -0600

    Setting up workshops domain

--- slate.config.ts
+++ slate.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from './src/helpers/config-helper';
 
 export default defineConfig({
   lang: 'en-US',
-  site: 'https://slate-blog-demo.vercel.app',
+  site: 'https://workshops.txconvergent.org/',
   avatar: '/txc.png',
   title: 'TX Convergent Workshops',
   description: 'Access all materials from past Convergent workshops',
